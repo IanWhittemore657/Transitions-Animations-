17ac548305ca436b24c56e1c5f514d27a85bdb14
IanWhittemore657 <59034906+IanWhittemore657@users.noreply.github.com>
2020-08-03 18:41:28 +0100

    Add files via upload

---
+++ android/app/src/main/kotlin/com/ianwhittemore/animationswithsimpleui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ianwhittemore.animationswithsimpleui
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
